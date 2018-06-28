7e89b606b55059a897ce272fda6e3e0ad772782f
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-06-27 22:48:42 -0700

    Adding the optimal solution

--- js/finders-keepers.js
+++ js/finders-keepers.js
@@ -20,5 +20,9 @@ function findElement (arr, func){
 }
 
 /** Optimal Solution */
+function findElementOptimal (arr, func){
+    return arr.find(func);
+}
 
 console.log(findElement([1, 3, 5, 8, 9, 10], function(num) {return num % 2 === 0}));
+console.log(findElementOptimal([1, 3, 5, 8, 9, 10], function(num) {return num % 2 === 0}));
